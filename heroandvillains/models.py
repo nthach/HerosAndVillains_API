a49bed45571554b4516670085e83e65407a77937
nthach <nich.thach@hotmail.com>
2022-08-29 14:45:36 -0700

    Created class in models.py

--- heroandvillains/models.py
+++ heroandvillains/models.py
@@ -1,3 +1,16 @@
+from pyexpat import model
+from xml.dom.minidom import CharacterData
 from django.db import models
 
 # Create your models here.
+
+class heroandvillains(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField()
+    secondary_ability = models.CharField()
+    catchphrase = CharacterData()
+    super_type = models.ForeignKey()
+    
+
+
